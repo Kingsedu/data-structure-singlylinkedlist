418d733a054b59d81b3a1210a542f794949fd8e3
kingsedu <chinedueh26@yahoo.com>
2025-11-21 19:29:21 +0100

    implement queue functionality

--- double.js
+++ double.js
@@ -158,27 +158,157 @@ class DoubleLinkedList {
     let removeNode;
     removeNode = this.get_method(0);
     if (!removeNode) return undefined;
-    if(this.l)
-    if (this.length === 1) {
-      removeNode = this.pop();
-      this.head = null;
-      this.tail = null;
-    } else {
-      let beforeNode = removeNode.previous;
-      let afterNode = removeNode.next;
-      removeNode.previous = null;
-      removeNode.next = null;
-      beforeNode.next = afterNode;
-      afterNode.previous = beforeNode;
-    }
+    if (this.l)
+      if (this.length === 1) {
+        removeNode = this.pop();
+        this.head = null;
+        this.tail = null;
+      } else {
+        let beforeNode = removeNode.previous;
+        let afterNode = removeNode.next;
+        removeNode.previous = null;
+        removeNode.next = null;
+        beforeNode.next = afterNode;
+        afterNode.previous = beforeNode;
+      }
 
     return removeNode;
   }
 }
 
-const doubleLinked = new DoubleLinkedList();
-doubleLinked.push("C");
-doubleLinked.push("H");
-doubleLinked.push("I");
-console.log("the removed tail", doubleLinked.pop());
-console.dir(doubleLinked, { depth: null, color: true });
+// const doubleLinked = new DoubleLinkedList();
+// doubleLinked.push("C");
+// doubleLinked.push("H");
+// doubleLinked.push("I");
+// console.log("the removed tail", doubleLinked.pop());
+// console.dir(doubleLinked, { depth: null, color: true });
+
+//**************************STACKS AND QUEUES ****************/
+//LINK -  STACK-- its follows a principle of LIFO-- data structure
+// the last element added to the stack will be the first element removed from the stack
+// building a stack,
+
+class StackNode {
+  constructor(val) {
+    this.next = null;
+    this.val = val;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  push(val) {
+    const new_stack_node = new StackNode(val);
+    if (this.length === 0) {
+      this.first = new_stack_node;
+      this.last = new_stack_node;
+    } else {
+      let firstFirstly = this.first;
+      this.first = new_stack_node;
+      this.first.next = firstFirstly;
+    }
+    return this.length++;
+  }
+
+  push_method(val) {
+    const new_stack_node = new StackNode(val);
+    if (this.length === 0) {
+      this.first = new_stack_node;
+      this.last = new_stack_node;
+    } else {
+      this.first.next = new_stack_node;
+      this.first = new_stack_node;
+    }
+    return this.length++;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
+}
+
+const stack = new Stack();
+// stack.push_method("The");
+// stack.push_method("Tech");
+// stack.push_method("Billionaire");
+// stack.push_method("in the making");
+// stack.push_method("********");
+// console.dir(stack, { depth: null, color: true });
+/* 
+the last
+    |
+    [the amount]-------- the first
+    when the length === 0 val = "The"
+    *********** *
+    the last
+    |
+    [The]-------- the first
+
+     *********** *
+    last
+    |
+    [Good]<------[the] <------first
+*/
+class QueueNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const new_node = new QueueNode(val);
+    if (this.size === 0) {
+      this.first = new_node;
+      this.last = new_node;
+    } else {
+      let temp = this.last;
+      temp.next = new_node;
+      this.last = new_node;
+    }
+    this.size++;
+    return this;
+  }
+  pop() {
+    if (this.size === 0) return null;
+    let temp = this.first;
+
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+      this.size--;
+      return temp;
+    }
+    this.first = this.first.next;
+    temp.next = null;
+    this.size--;
+    return temp;
+  }
+}
+
+const queue = new Queue();
+queue.push("C");
+queue.push("H");
+queue.push("I");
+queue.push("N");
+queue.push("E");
+queue.push("D");
+console.log(queue.pop());
+console.dir(queue, { depth: null, color: true });
