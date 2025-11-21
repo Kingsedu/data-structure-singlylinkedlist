620939f819f78c982e7a162eddad31f27bcf8fe3
kingsedu <chinedueh26@yahoo.com>
2025-11-21 12:47:17 +0100

    double-linked

---
+++ double.js
@@ -0,0 +1,184 @@
+// doing the double linked list, the difference is that it has a previous
+
+class Node {
+  constructor(num) {
+    this.num = num;
+    this.previous = null;
+    this.next = null;
+  }
+}
+
+class DoubleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val); // creating a new node
+    if (this.length === 0) {
+      // checking if the length is 0
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      let oldTail = this.tail;
+      this.tail.next = newNode;
+      newNode.previous = oldTail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    let removedTail = this.tail;
+    if (this.length === 0) return null;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return removedTail;
+    }
+    let newTail = removedTail.previous;
+    newTail.next = null;
+    this.tail = newTail;
+    this.length--;
+    return removedTail;
+  }
+  shift() {
+    if (this.length === 0) return null;
+    let removedHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let newHead = this.head.next;
+      newHead.previous = null;
+      removedHead.next = null;
+      this.head = newHead;
+    }
+    this.length--;
+    return removedHead;
+  }
+  unshift(val) {
+    const new_Head = new Node(val);
+    let oldHead = this.head;
+    if (this.length === 0) {
+      this.head = new_Head;
+      this.tail = new_Head;
+    } else {
+      oldHead.previous = new_Head;
+      new_Head.next = oldHead;
+      this.head = new_Head;
+    }
+    this.length++;
+    return this;
+  }
+  //FIXME -
+  get(index, num) {
+    const numberLength = this.length;
+    let count = 0;
+    //takes an index and a number and returns the node at that index
+    if (index < 0 || index > this.length) return null;
+    if (index <= numberLength / 2) {
+      let current = this.head;
+      let stop = true;
+      while (current && stop) {
+        if (current.num !== num) {
+          current = current.next;
+          count++;
+        } else {
+          stop = !stop;
+        }
+      }
+      return current;
+    }
+    if (index >= numberLength / 2) {
+      let prev = this.tail;
+      let stop = true;
+      while (prev && stop) {
+        if (prev.num !== num) {
+          prev = prev.previous;
+          count++;
+        } else {
+          stop = false;
+        }
+      }
+      return prev;
+    }
+  }
+
+  get_method(index) {
+    if (index < 0 || index >= this.length) return null;
+    if (index <= this.length / 2) {
+      let count = 0;
+
+      let current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+      return current;
+    } else {
+      let count = this.length - 1;
+      let current = this.tail;
+      while (count !== index) {
+        current = current.previous;
+        count--;
+      }
+      return current;
+    }
+  }
+
+  set(index, val) {
+    let foundNode = this.get_method(index);
+    if (foundNode !== null) {
+      foundNode.num = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(val);
+    if (index === this.length) return this.push(val);
+    const newNode = new Node(val);
+    let beforeNode = this.get_method(index - 1);
+    let afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    let removeNode;
+    removeNode = this.get_method(0);
+    if (!removeNode) return undefined;
+    if(this.l)
+    if (this.length === 1) {
+      removeNode = this.pop();
+      this.head = null;
+      this.tail = null;
+    } else {
+      let beforeNode = removeNode.previous;
+      let afterNode = removeNode.next;
+      removeNode.previous = null;
+      removeNode.next = null;
+      beforeNode.next = afterNode;
+      afterNode.previous = beforeNode;
+    }
+
+    return removeNode;
+  }
+}
+
+const doubleLinked = new DoubleLinkedList();
+doubleLinked.push("C");
+doubleLinked.push("H");
+doubleLinked.push("I");
+console.log("the removed tail", doubleLinked.pop());
+console.dir(doubleLinked, { depth: null, color: true });
